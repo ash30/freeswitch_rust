71930e148963dd34aa1a8999fb2ef73deec44c70
Ashley Arthur <ashleyarthur98@gmail.com>
2024-09-02 10:24:25 +0100

    test mod

---
+++ mods/mod_hello_world/src/lib.rs
@@ -0,0 +1,20 @@
+use freeswitch_rs::log::{info, debug};
+use freeswitch_rs::SWITCH_CHANNEL_ID_LOG;
+use freeswitch_rs::*;
+
+switch_module_define!(mod_audiofork, load);
+
+#[switch_module_load_function]
+fn load(module: FSModuleInterface, pool: FSModulePool) -> switch_status_t {
+    info!(channel = SWITCH_CHANNEL_ID_LOG; "mod hello_world loading");
+    module.add_api(api_main);
+    return switch_status_t::SWITCH_STATUS_SUCCESS
+}
+
+#[switch_api_define("hello_world")]
+fn api_main(cmd:&str, _session:Option<Session>, mut stream:StreamHandle) -> switch_status_t {
+    debug!(channel = SWITCH_CHANNEL_ID_SESSION; "mod hello_world cmd {}", &cmd);
+    let _ = writeln!(stream, "+OK Success");
+    switch_status_t::SWITCH_STATUS_SUCCESS
+}
+
