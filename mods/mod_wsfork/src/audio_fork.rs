a30c17fa6540249139cdc6337b18b8337196e116
Ashley Arthur <ashleyarthur98@gmail.com>
2025-09-10 14:59:01 +0100

    wip: mod_ws new impl

---
+++ mods/mod_wsfork/src/audio_fork.rs
@@ -0,0 +1,143 @@
+use anyhow::Result;
+use fastwebsockets::handshake;
+use fastwebsockets::Frame;
+use http_body_util::Empty;
+use hyper::{
+    body::Bytes,
+    header::{CONNECTION, UPGRADE},
+    Request,
+};
+
+use hyper_util::rt::TokioExecutor;
+use ringbuf::traits::Consumer;
+use ringbuf::traits::Producer;
+use ringbuf::traits::Split;
+use ringbuf::HeapRb;
+use std::io::Read;
+use std::sync::Arc;
+use tokio::io::AsyncRead;
+use tokio::io::AsyncWrite;
+use tokio::sync::Notify;
+
+type WSRequest = Request<Empty<Bytes>>;
+
+pub fn create_request(url: String, customise: impl FnOnce(&mut WSRequest)) -> Result<WSRequest> {
+    Request::builder()
+        .method("GET")
+        .uri(url)
+        .header(UPGRADE, "websocket")
+        .header(CONNECTION, "upgrade")
+        .header(
+            "Sec-WebSocket-Key",
+            fastwebsockets::handshake::generate_key(),
+        )
+        .header("Sec-WebSocket-Version", "13")
+        .body(Empty::new())
+        .map_err(|e| e.into())
+}
+
+pub struct AudioFork<B> {
+    req: WSRequest,
+    read: Arc<Notify>,
+    stop: Arc<Notify>,
+    buf: B,
+}
+
+// Note: 4 candles!!!
+pub struct AudioForkHandle<B> {
+    read: Arc<Notify>,
+    stop: Arc<Notify>,
+    buf: B,
+}
+
+impl<B> AudioForkHandle<B>
+where
+    B: Producer<Item = u8>,
+{
+    pub fn write<R>(&mut self, src: &mut R) -> Option<std::io::Result<usize>>
+    where
+        R: Read,
+    {
+        let res = self.buf.read_from(src, None);
+        if res.is_some() {
+            self.read.notify_waiters()
+        };
+        res
+    }
+
+    pub fn close(&self) {
+        self.stop.notify_waiters();
+    }
+}
+
+pub fn new_fork(
+    req: WSRequest,
+    buf_size: usize,
+) -> (
+    AudioForkHandle<impl Producer<Item = u8>>,
+    AudioFork<impl Consumer<Item = u8>>,
+) {
+    let stop = Arc::new(Notify::new());
+    let read = Arc::new(Notify::new());
+
+    let rb = HeapRb::<u8>::new(buf_size);
+    let (buf_tx, buf_rx) = rb.split();
+
+    (
+        AudioForkHandle {
+            buf: buf_tx,
+            stop: stop.clone(),
+            read: read.clone(),
+        },
+        AudioFork {
+            req,
+            stop: stop.clone(),
+            read: read.clone(),
+            buf: buf_rx,
+        },
+    )
+}
+
+impl<B> AudioFork<B>
+where
+    B: Consumer<Item = u8>,
+{
+    pub async fn run<S>(self, stream: S) -> Result<()>
+    where
+        S: AsyncRead + AsyncWrite + Send + Unpin + 'static,
+    {
+        let exector = TokioExecutor::new();
+        let (mut ws, res) = handshake::client(&exector, self.req, stream).await?;
+        ws.set_auto_close(true);
+
+        loop {
+            tokio::select! {
+                _ = self.stop.notified() => {
+                    // TODO: WHAT REASON
+                    ws.write_frame(Frame::close(0, b"")).await;
+                    break
+                },
+                _ = self.read.notified() => {
+                    // READ ALL
+                    let slices = self.buf.as_slices();
+                    for b in [slices.0, slices.1] {
+                        // how much samples DO we want to send ?
+                        let sample_rate = 8000;
+                        let frame_payload_size = 2 * ( sample_rate / 1000) * 20;
+                        b.chunks(frame_payload_size).for_each( | data | {
+                            // TODO:
+                            ws.write_frame(Frame::binary(fastwebsockets::Payload::Borrowed(data)));
+                            unsafe {
+                                self.buf.advance_read_index(data.len());
+                            }
+
+                        });
+                    }
+
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
--- mods/mod_wsfork/src/lib.rs
+++ mods/mod_wsfork/src/lib.rs
@@ -1,14 +1,23 @@
-use std::borrow::Borrow;
-use std::borrow::BorrowMut;
-use anyhow::Result;
+mod audio_fork;
+
+use audio_fork::create_request;
+use audio_fork::new_fork;
+
 use anyhow::anyhow;
+use anyhow::Result;
 use clap::{Command, FromArgMatches as _, Parser, Subcommand as _};
 
-use freeswitch_rs::log::{info, debug};
+use std::sync::LazyLock;
+use tokio::net::TcpStream;
+
+use freeswitch_rs::log::{debug, info};
 use freeswitch_rs::*;
-use ws::WSForker;
+use tokio::runtime::Runtime;
 
 const BUG_CHANNEL_KEY: &core::ffi::CStr = c"_WS_FORK_BUG";
+
+static RT: LazyLock<Runtime> =
+    LazyLock::new(|| tokio::runtime::Builder::new_multi_thread().build().unwrap());
 
 #[derive(Parser, Debug)]
 enum Subcommands {
@@ -17,10 +26,7 @@ enum Subcommands {
         session: String,
 
         #[arg()]
-        ip: String,
-
-        #[arg()]
-        port: u16,
+        url: String,
     },
     Stop {
         #[arg()]
@@ -28,7 +34,7 @@ enum Subcommands {
     },
 }
 
-fn parse_args(cmd_str:&str) -> Result<Subcommands> {
+fn parse_args(cmd_str: &str) -> Result<Subcommands> {
     let mut cmd = Command::new("argparse")
         .disable_version_flag(true)
         .disable_help_flag(true)
@@ -51,70 +57,91 @@ impl LoadableModule for FSMod {
 }
 
 #[switch_api_define("ws_fork")]
-fn api_main(cmd:&str, _session:Option<Session>, mut stream:StreamHandle) -> switch_status_t {
+fn api_main(cmd: &str, _session: Option<Session>, mut stream: StreamHandle) -> switch_status_t {
     debug!(channel=SWITCH_CHANNEL_ID_SESSION; "mod audiofork cmd {}", &cmd);
     match parse_args(cmd) {
         Err(_) => {
             let _ = write!(stream, "ERR: mod audiofork invalid usage");
-        },
+        }
         Ok(cmd) => {
             let _ = match cmd {
-                Subcommands::Start { session, ip, port} => api_start(session, ip, port),
-                Subcommands::Stop { session } => api_stop(session)
+                Subcommands::Start { session, url } => api_start(session, url),
+                Subcommands::Stop { session } => api_stop(session),
             };
         }
     }
     switch_status_t::SWITCH_STATUS_SUCCESS
 }
 
-fn api_stop(uuid:String) -> Result<()>{
+fn api_stop(uuid: String) -> Result<()> {
     let s = Session::locate(&uuid).ok_or(anyhow!("Session Not Found: {}", uuid))?;
     let mut c = s.get_channel();
     // TODO: Can we make this safer?
     let Some(bug) = c.get_private_unsafe(BUG_CHANNEL_KEY) else {
-        return Err(anyhow!("Bug Not Found: {}", uuid))
+        return Err(anyhow!("Bug Not Found: {}", uuid));
     };
     s.remove_media_bug(bug);
     Ok(())
 }
 
-fn api_start(uuid:String, ip:String, port:u16) -> Result<()> {
+fn api_start(uuid: String, url: String) -> Result<()> {
     debug!(channel=SWITCH_CHANNEL_ID_SESSION; "mod audiofork start uuid:{}",uuid);
 
-    // We can locate session and have RAII guard unlock for us when scope finishes
-    let s = Session::locate(&uuid).ok_or(anyhow!("Session Not Found: {}", uuid))?;
-    let mut forker = WSForker::new(SWITCH_RECOMMENDED_BUFFER_SIZE);
-    forker.connect(ip, port)?;
+    let req = create_request(url.clone(), |req| {})?;
+    let (mut handle, fork) = new_fork(req, SWITCH_RECOMMENDED_BUFFER_SIZE);
 
-    let bug = s.add_media_bug("".to_string(), "".to_string(), 0, move |mut bug, abc_type| { 
-        // For error handling, if we return false from closure
-        // FS will prune mal functioning bug ( ie remove it ) 
-        let mut should_continue = true;
-        match abc_type {
-            switch_abc_type_t::SWITCH_ABC_TYPE_INIT => {}
-            switch_abc_type_t::SWITCH_ABC_TYPE_CLOSE => forker.close(),
-            switch_abc_type_t::SWITCH_ABC_TYPE_READ => {
-                let mut b = FrameBuffer::default();
-                should_continue = loop {
-                    // read data from bug until FS returns non success 
-                    let Ok(n) = bug.read_frame(b.borrow_mut()) else { break true };
-                    // in theory, `n` should be `decoded_bytes_per_packet` size
-                    if n == 0 { break true }
-                    if !forker.fork(b.borrow()) { break false } else { continue };
-                };
-            }
-            _ => {}
+    let s = Session::locate(&uuid).ok_or(anyhow!("Session Not Found: {}", uuid))?;
+    let bug = s.add_media_bug(
+        "".to_string(),
+        "".to_string(),
+        0,
+        move |mut bug, abc_type| {
+            // For error handling, if we return false from closure
+            // FS will prune mal functioning bug ( ie remove it )
+            let mut should_continue = true;
+            match abc_type {
+                switch_abc_type_t::SWITCH_ABC_TYPE_INIT => {}
+                switch_abc_type_t::SWITCH_ABC_TYPE_CLOSE => handle.close(),
+                switch_abc_type_t::SWITCH_ABC_TYPE_READ => {
+                    should_continue = loop {
+                        // read data from bug until FS returns non success
+                        match handle.write(&mut bug) {
+                            None => {} // buffer was full ... flush
+                            Some(Ok(n)) => {
+                                if n == 0 {
+                                    break true; // nothing left
+                                }
+                                continue;
+                            }
+                            Some(Err(e)) => {
+                                // AN IO Error .... fail fast?
+                                // notify via event
+                                break false;
+                            }
+                        }
+                    };
+                }
+                _ => {}
+            };
+            should_continue
+        },
+    );
+
+    // run forker in background
+    tokio::spawn(async move {
+        // TODO: Errors
+        let Ok(stream) = TcpStream::connect(url).await else {
+            // TOD
+            return;
         };
-        should_continue
+
+        if let Err(e) = fork.run(stream).await {
+            // send event
+        }
     });
 
-    // TODO: handle bug result
     let mut channel = s.get_channel();
     channel.set_private(BUG_CHANNEL_KEY, bug.unwrap());
 
     Ok(())
 }
-
-
-
-
