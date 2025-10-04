2c97a8adfd85635d29ac7b6c95d5bf273ff9efb2
Ashley Arthur <ashleyarthur98@gmail.com>
2025-10-04 10:12:59 +0100

    feature: raw ptr channel api
    ergonomic escape hatch really, marked unsafe since caller will have
    to take care of lifetimes

--- freeswitch_rs/src/session.rs
+++ freeswitch_rs/src/session.rs
@@ -148,7 +148,15 @@ fs_session_owned_type!(Channel, *mut switch_channel_t);
 type ChannelStateHandlerIndex = usize;
 
 impl<'a> Channel<'a> {
-    pub fn set_private_with_key<T: IntoChannelValue>(&self, key: &CStr, data: T) -> Result<()> {
+    // We should treat values within a channel as 'shared'
+    // since the channels themselve make no claim over ownership.
+    // To this end, we require values to implement clone
+    // to avoid inadverntanly invalidating the ptr stored.
+    pub fn set_private<T>(&self, key: &CStr, data: T) -> Result<()>
+    where
+        T: IntoChannelValue + Clone,
+        T: 'a, // data must be equal or outlive channel/session
+    {
         let data_ptr = data.into_value();
 
         // SAFETY:
@@ -162,10 +170,41 @@ impl<'a> Channel<'a> {
         }
     }
 
+    pub fn get_private<T>(&self, key: &CStr) -> Option<T>
+    where
+        T: IntoChannelValue + Clone,
+        T: 'a, // data must be equal or outlive channel/session
+    {
+        unsafe {
+            let ptr = self.get_private_raw_ptr(key)?;
+            let t = T::from_value(ptr);
+            let ret = Some(t.clone());
+            t.into_value();
+            ret
+        }
+    }
+
     /// # Safety
     ///
-    /// Callers must enure channel key was set to T previously
-    pub unsafe fn get_private_with_key<T: IntoChannelValue>(&self, key: &CStr) -> Option<&T> {
+    /// Channels do not own or cleanup their data, so caller must ensure ptrs
+    /// to rust allocated structs are cleaned up IF required ie call drop!
+    pub unsafe fn set_private_raw_ptr<T>(&self, key: &CStr, data: *const T) -> Result<()> {
+        // SAFETY:
+        // FS will take a lock on channel insert / read so its safe to call
+        // with shared reference
+        unsafe {
+            match switch_channel_set_private(self.as_ptr(), key.as_ptr(), data as *const c_void) {
+                switch_status_t::SWITCH_STATUS_SUCCESS => Ok(()),
+                other => Err(other.into()),
+            }
+        }
+    }
+
+    /// # Safety
+    ///
+    /// Care must be taken to not invalidate the stored pointer whilst the channel holds the value
+    /// ie ensure T is not dropped
+    pub unsafe fn get_private_raw_ptr<T>(&self, key: &CStr) -> Option<*mut T> {
         // SAFETY:
         // FS will take a lock on channel insert / read so its safe to call
         // with shared reference
@@ -174,7 +213,7 @@ impl<'a> Channel<'a> {
             if ptr.is_null() {
                 return None;
             }
-            Some(&*(ptr as *const T))
+            Some(ptr as *mut T)
         }
     }
 
